3ea2069f9a2e1c9769938ee8f8f36917e3b746f6
nicolascarreno <ncarreno@fi.uba.ar>
2023-11-25 14:05:32 -0300

    Ventana Error

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,23 @@
+import React from 'react';
+import Link from "next/link";
+
 export default function NotFoundLayout() {
   return (
     <div className="flex h-full flex-col justify-center items-center bg-white">
+      <img
+        src="Error.jpg"
+        alt="Error"
+      />
+
       <h1 className="text-4xl mb-5 font-bold">
         Hubo un error al realizar la operación
       </h1>
+      <Link
+        className="bg-sky-500	hover:bg-cyan-600 text-white font-bold py-1 px-4 rounded"
+        href="/home"
+       >
+        Volver a inicio
+       </Link>
       <span className="text-7xl"></span>
     </div>
   );

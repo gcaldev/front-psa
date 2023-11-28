66714800196816a00804e9193ce0843b5cd9d321
nicolascarreno <ncarreno@fi.uba.ar>
2023-11-28 16:50:22 -0300

    Ventanas OK

--- pages/ok_info_proyecto.tsx
+++ pages/ok_info_proyecto.tsx
@@ -5,18 +5,25 @@ export default function NotFoundLayout() {
   return (
     <div className="flex h-full flex-col justify-center items-center bg-white">
       <img
-        src="error.jpg"
-        alt="Error"
+        src="ok.jpg"
+        alt="Ok"
+        style={{marginBottom: "25px"}}
       />
 
       <h1 className="text-4xl mb-5 font-bold" style={{marginBottom: "35px"}}>
-        Hubo un error al realizar la operación
+        Info guardada exitosamente
       </h1>
       <Link
         className="bg-sky-500	hover:bg-cyan-600 text-white font-bold py-1 px-4 rounded"
         href="/home"
        >
-        Volver a inicio
+        Ver proyecto
+       </Link>
+       <Link
+        className="bg-white-500 hover:bg-grey-600 text-sky-500 font-bold py-1 px-4 rounded"
+        href="/home"
+       >
+        Volver a listado de proyectos
        </Link>
       <span className="text-7xl"></span>
     </div>

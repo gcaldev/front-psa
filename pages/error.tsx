86f80d51586d60d8c893d93fdba9017e2cf959eb
nicolascarreno <ncarreno@fi.uba.ar>
2023-11-28 19:32:38 -0300

    upadate ventana error

--- pages/error.tsx
+++ pages/error.tsx
@@ -1,6 +1,10 @@
 export default function Error() {
   return (
     <div className="flex h-full flex-col justify-center items-center bg-white">
+      <img
+        src="error.jpg"
+        alt="Error"
+      />
       <h1 className="text-4xl mb-5 font-bold">
         Hubo un error al realizar la operación
       </h1>

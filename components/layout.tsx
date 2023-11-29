4985f63b93997732a12c07c504a3fa1d1d0a199e
nicolascarreno <ncarreno@fi.uba.ar>
2023-11-29 00:27:34 -0300

    cambio fondo a celeste

--- components/layout.tsx
+++ components/layout.tsx
@@ -29,7 +29,7 @@ export default function Layout({ children }: { children: any }) {
 
   return (
     <div className="min-h-screen flex flex-col bg-white">
-      <header className="bg-black sticky top-0 h-14 flex  justify-between p-4 items-center font-semibold uppercase text-white">
+      <header className="bg-sky-500 sticky top-0 h-14 flex  justify-between p-4 items-center font-semibold uppercase text-white">
         <h1>PSA</h1>
         <ul className="flex gap-8">
           <li>

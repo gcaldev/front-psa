257d7120acca70cc3feaa50bb9e0326fa8be3997
Gonzalo <gcalderon@fi.uba.ar>
2023-11-30 01:13:11 -0300

    feat: agrego validacion formulario tarea

--- components/TaskLayout.tsx
+++ components/TaskLayout.tsx
@@ -5,6 +5,16 @@ import { useEffect, useState } from "react";
 
 const DEFAULT_SELECT_VALUE = "Elegir";
 
+type InfoValidationType = {
+  fechaInicio: boolean;
+  fechaFin: boolean;
+  estado: boolean;
+  prioridad: boolean;
+  asignado: boolean;
+  titulo: boolean;
+  descripcion: boolean;
+};
+
 export default function TaskLayout({
   id = "",
   project_id,
@@ -14,6 +24,16 @@ export default function TaskLayout({
 }) {
   const createsTask = !id;
   const router = useRouter();
+  const [infoValidation, setInfoValidation] = useState<InfoValidationType>({
+    fechaInicio: false,
+    fechaFin: false,
+    estado: false,
+    prioridad: false,
+    asignado: false,
+    titulo: false,
+    descripcion: false,
+  });
+
   const [taskInfo, setTaskInfo] = useState({
     project_id,
     fechaInicio: "",
@@ -82,21 +102,51 @@ export default function TaskLayout({
   }, []);
 
   if (isLoading) {
-    return(
-        <div className="text-center py-40">
-            <div role="status">
-                <svg aria-hidden="true" className="inline w-22 h-22 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                </svg>
-                <span className="sr-only">Loading...</span>
-            </div>
-            <h1 className="text-2xl font-semibold mt-5">Cargando...</h1>
+    return (
+      <div className="text-center py-40">
+        <div role="status">
+          <svg
+            aria-hidden="true"
+            className="inline w-22 h-22 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
         </div>
+        <h1 className="text-2xl font-semibold mt-5">Cargando...</h1>
+      </div>
     );
   }
 
   const handleTaskSubmit = () => {
+    const validations = {
+      estado: taskInfo.estado === DEFAULT_SELECT_VALUE,
+      asignado: taskInfo.asignado === DEFAULT_SELECT_VALUE,
+      prioridad: taskInfo.prioridad === DEFAULT_SELECT_VALUE,
+      fechaInicio: new Date(taskInfo.fechaInicio) > new Date(taskInfo.fechaFin),
+      fechaFin: false,
+      titulo: !taskInfo.titulo,
+      descripcion: !taskInfo.descripcion,
+    };
+
+    setInfoValidation(validations);
+
+    const invalidForm = Object.values(validations).some(
+      (validation) => validation
+    );
+
+    if (invalidForm) return;
+
     setIsLoading(true);
 
     const method = createsTask ? "POST" : "PUT";
@@ -140,6 +190,11 @@ export default function TaskLayout({
               setTaskInfo((prev) => ({ ...prev, titulo: e.target.value }))
             }
           />
+          {infoValidation.titulo && (
+            <label className="block mt-2 text-sm font-medium text-red-500">
+              Completa un titulo
+            </label>
+          )}
         </div>
         <div className="col-span-3">
           <label className="block mb-2 text-sm font-medium text-gray-900">
@@ -155,6 +210,11 @@ export default function TaskLayout({
             }
             value={taskInfo.fechaInicio}
           />
+          {infoValidation.fechaInicio && (
+            <label className="block mt-2 text-sm font-medium text-red-500">
+              La fecha de inicio debe ser previa a la de finalización
+            </label>
+          )}
         </div>
         <div className="col-span-3">
           <label className="block mb-2 text-sm font-medium text-gray-900">
@@ -189,6 +249,11 @@ export default function TaskLayout({
             <option value="Finalizado">Terminado</option>
             <option value="Bloqueado">Bloqueado</option>
           </select>
+          {infoValidation.estado && (
+            <label className="block mt-2 text-sm font-medium text-red-500">
+              Selecciona un estado
+            </label>
+          )}
         </div>
         <div className="col-span-2">
           <label className="block mb-2 text-sm font-medium text-gray-900">
@@ -207,6 +272,11 @@ export default function TaskLayout({
             <option value="Media">Media</option>
             <option value="Alta">Alta</option>
           </select>
+          {infoValidation.prioridad && (
+            <label className="block mt-2 text-sm font-medium text-red-500">
+              Selecciona una prioridad
+            </label>
+          )}
         </div>
         <div className="col-span-2">
           <label className="block mb-2 text-sm font-medium text-gray-900">
@@ -230,6 +300,11 @@ export default function TaskLayout({
               );
             })}
           </select>
+          {infoValidation.asignado && (
+            <label className="block mt-2 text-sm font-medium text-red-500">
+              Selecciona un asignado
+            </label>
+          )}
         </div>
         <div className="col-span-6">
           <label className="block mb-2 text-sm font-medium text-gray-900">
@@ -243,6 +318,11 @@ export default function TaskLayout({
             }
             value={taskInfo.descripcion}
           ></textarea>
+          {infoValidation.descripcion && (
+            <label className="block mt-2 text-sm font-medium text-red-500">
+              Completa una descripción
+            </label>
+          )}
         </div>
       </div>
       <div></div>

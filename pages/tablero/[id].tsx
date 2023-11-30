ca9e056e487afd79678c291ade931f9b6e5fa59e
Gonzalo <gcalderon@fi.uba.ar>
2023-11-30 18:32:59 -0300

    fix: fetch de tablero corregido

--- pages/tablero/[id].tsx
+++ pages/tablero/[id].tsx
@@ -93,17 +93,29 @@ export default function TableroLayout() {
   }, [router.isReady]);
 
   if (loading || deleteLoading) {
-    return(
-        <div className="text-center py-40">
-            <div role="status">
-                <svg aria-hidden="true" className="inline w-22 h-22 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                </svg>
-                <span className="sr-only">Loading...</span>
-            </div>
-            <h1 className="text-2xl font-semibold mt-5">Cargando...</h1>
+    return (
+      <div className="text-center py-40">
+        <div role="status">
+          <svg
+            aria-hidden="true"
+            className="inline w-22 h-22 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span className="sr-only">Loading...</span>
         </div>
+        <h1 className="text-2xl font-semibold mt-5">Cargando...</h1>
+      </div>
     );
   }
 
@@ -149,7 +161,7 @@ export default function TableroLayout() {
       .then((res) => res.json())
       .then((res) => {
         setDeleteLoading(false);
-        fetchData();
+        fetchData(url, "GET");
         onSuccess?.();
       })
       .catch((err) => router.push("/error"));
@@ -206,7 +218,9 @@ export default function TableroLayout() {
       >
         <div className="grid grid-cols-2 px-2 py-2 mt-2 border border-black p-4">
           <div className="flex w-80 mb-5">
-            <h2 className="col-span-2 text-2xl font-semibold">{selectedTask?.titulo}</h2>
+            <h2 className="col-span-2 text-2xl font-semibold">
+              {selectedTask?.titulo}
+            </h2>
           </div>
 
           <p className="col-span-2 font-semibold">Descripción</p>
@@ -219,19 +233,19 @@ export default function TableroLayout() {
 
           {selectedTask?.fechaInicio && (
             <div>
-                <p className="font-semibold">Fecha de inicio</p>
-                <p className="mb-5">{selectedTask.fechaInicio}</p>
+              <p className="font-semibold">Fecha de inicio</p>
+              <p className="mb-5">{selectedTask.fechaInicio}</p>
             </div>
           )}
           {selectedTask?.fechaInicio && (
             <div>
-                <p className="font-semibold">Fecha de fin</p>
-                <p className="mb-5">{selectedTask.fechaFin}</p>
+              <p className="font-semibold">Fecha de fin</p>
+              <p className="mb-5">{selectedTask.fechaFin}</p>
             </div>
           )}
           <div className="flex mb-5">
-              <p className="font-semibold mr-2">Asignada a</p>
-              <p className="col-span-2">{selectedTask?.asignado}</p>
+            <p className="font-semibold mr-2">Asignada a</p>
+            <p className="col-span-2">{selectedTask?.asignado}</p>
           </div>
         </div>
         <div className="flex flex-1 justify-end items-end gap-8">
@@ -262,14 +276,14 @@ export default function TableroLayout() {
       >
         <div className="flex flex-col">
           <div className="border border-black p-4 mt-2">
-          <p className="font-bold mb-4 mt-4 text-center">
-            ¿Está seguro de que desea eliminar la tarea seleccionada?
-          </p>
-          <p className="mb-5">
-            Esta acción no se puede deshacer y toda la información asociada a
-            esta tarea se perderá. Por favor, confirme su decisión antes de
-            proceder.
-          </p>
+            <p className="font-bold mb-4 mt-4 text-center">
+              ¿Está seguro de que desea eliminar la tarea seleccionada?
+            </p>
+            <p className="mb-5">
+              Esta acción no se puede deshacer y toda la información asociada a
+              esta tarea se perderá. Por favor, confirme su decisión antes de
+              proceder.
+            </p>
           </div>
           <div className="flex flex-1 justify-end mt-7 items-end gap-8 items-center">
             <button

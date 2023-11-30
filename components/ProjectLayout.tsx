be4ae3da83686ffde31da42c959b5048d5dc421f
Gonzalo <gcalderon@fi.uba.ar>
2023-11-29 23:08:27 -0300

    feat: agrego logica basica a pantallas de proyecto

--- components/ProjectLayout.tsx
+++ components/ProjectLayout.tsx
@@ -1,34 +1,21 @@
-import { Recurso, Tarea } from "@/types/types";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { Proyecto, Recurso } from "@/types/types";
 
 const DEFAULT_SELECT_VALUE = "Elegir";
 
-export default function TaskLayout({
-  id = "",
-  project_id,
-}: {
-  id?: string;
-  project_id: string;
-}) {
-  const createsTask = !id;
+export default function ProjectLayout({ id = "" }: { id?: string }) {
   const router = useRouter();
-  const [taskInfo, setTaskInfo] = useState({
-    project_id,
+  const createsProject = !id;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [projectInfo, setProjectInfo] = useState<Proyecto>({
+    nombre: "",
     fechaInicio: "",
     fechaFin: "",
-    estado: DEFAULT_SELECT_VALUE,
-    prioridad: DEFAULT_SELECT_VALUE,
-    asignado: DEFAULT_SELECT_VALUE,
-    titulo: "",
     id: "",
-    descripcion: "",
-  });
-  const [isLoading, setIsLoading] = useState<boolean>(!createsTask);
-  console.log({
-    id,
-    project_id,
+    estado: DEFAULT_SELECT_VALUE,
+    lider: DEFAULT_SELECT_VALUE,
   });
   const [resources, setResources] = useState<Recurso[]>([]);
 
@@ -53,7 +40,7 @@ export default function TaskLayout({
   }, []);
 
   useEffect(() => {
-    if (createsTask) {
+    if (createsProject) {
       return;
     }
     setIsLoading(true);
@@ -64,32 +51,25 @@ export default function TaskLayout({
         "Content-Type": "application/json",
       },
     };
-    const url = `https://my-json-server.typicode.com/gcaldev/psa-mock/tareas/${id}`;
+    const url = `https://my-json-server.typicode.com/gcaldev/psa-mock/proyectos/${router.query.id}`;
 
     fetch(url, options)
       .then((res) => res.json())
       .then((res) => {
-        const { fechaInicio, fechaFin, ...rest } = res;
-        setTaskInfo({
-          ...rest,
-          fechaInicio: fechaInicio ?? "",
-          fechaFin: fechaFin ?? "",
-        });
-        console.log(res);
+        setProjectInfo(res);
         setIsLoading(false);
       })
       .catch((err) => router.push("/error"));
-  }, []);
+  }, [router.isReady]);
 
-  if (isLoading) {
-    return <h1>Cargando...</h1>;
-  }
-
-  const handleTaskSubmit = () => {
+  const handleProjectSubmit = () => {
     setIsLoading(true);
 
-    const method = createsTask ? "POST" : "PUT";
-    const body = createsTask ? { ...taskInfo, id: undefined } : taskInfo;
+    const method = createsProject ? "POST" : "PUT";
+    const body = createsProject
+      ? { ...projectInfo, id: undefined }
+      : projectInfo;
+
     const options = {
       method,
       headers: {
@@ -98,7 +78,7 @@ export default function TaskLayout({
       body: JSON.stringify(body),
     };
     const url = `https://my-json-server.typicode.com/gcaldev/psa-mock/proyectos/${id}`;
-    console.log(options, "INFO");
+
     fetch(url, options)
       .then((res) => res.json())
       .then((res) => {
@@ -107,22 +87,40 @@ export default function TaskLayout({
       .catch((err) => router.push("/error"));
   };
 
+  if (isLoading) {
+    return <h1>Cargando...</h1>;
+  }
+
   return (
-    <div className="flex-1 justify-center">
-      <h1 className="text-3xl font-bold">
-        {createsTask ? "Crear tarea" : "Editar tarea"}
-      </h1>
+    <div className="flex-1 max-w-screen-md	justify-center mt-1">
+      <div className="flex border">
+        <img src="../info40.png" alt="info" className="mt-12 mb-12 ml-10" />
+        {createsProject ? (
+          <h1 className="text-3xl font-bold mb-10 mt-12 ml-5">
+            Crear proyecto
+          </h1>
+        ) : (
+          <>
+            <h1 className="text-3xl font-bold mb-10 mt-12 ml-5">
+              Información Del Proyecto -
+            </h1>
+            <h1 className="text-3xl font-bold mb-10 mt-12 ml-2">
+              {projectInfo?.nombre}
+            </h1>
+          </>
+        )}
+      </div>
       <div className="grid grid-cols-6 gap-4 mt-8">
         <div className="col-span-6">
           <label className="block mb-2 text-sm font-medium text-gray-900">
-            Titulo
+            Nombre proyecto
           </label>
           <input
             type="text"
-            value={taskInfo.titulo}
+            value={projectInfo.nombre}
             className="bg-gray-50 w-full border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5 "
             onChange={(e) =>
-              setTaskInfo((prev) => ({ ...prev, titulo: e.target.value }))
+              setProjectInfo((prev) => ({ ...prev, nombre: e.target.value }))
             }
           />
         </div>
@@ -136,9 +134,12 @@ export default function TaskLayout({
             name="trip-start"
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
             onChange={(e) =>
-              setTaskInfo((prev) => ({ ...prev, fechaInicio: e.target.value }))
+              setProjectInfo((prev) => ({
+                ...prev,
+                fechaInicio: e.target.value,
+              }))
             }
-            value={taskInfo.fechaInicio}
+            value={projectInfo.fechaInicio}
           />
         </div>
         <div className="col-span-3">
@@ -151,58 +152,39 @@ export default function TaskLayout({
             name="trip-start"
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
             onChange={(e) =>
-              setTaskInfo((prev) => ({ ...prev, fechaFin: e.target.value }))
+              setProjectInfo((prev) => ({ ...prev, fechaFin: e.target.value }))
             }
-            value={taskInfo.fechaFin}
+            value={projectInfo.fechaFin}
           />
         </div>
-        <div className="col-span-2">
+        <div className="col-span-3">
           <label className="block mb-2 text-sm font-medium text-gray-900">
             Estado
           </label>
           <select
             id="countries"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            value={taskInfo.estado}
+            value={projectInfo.estado}
             onChange={(e) =>
-              setTaskInfo((prev) => ({ ...prev, estado: e.target.value }))
+              setProjectInfo((prev) => ({ ...prev, estado: e.target.value }))
             }
           >
             <option value={DEFAULT_SELECT_VALUE}>Elegir</option>
             <option value="Sin Comenzar">Sin comenzar</option>
             <option value="En Progreso">En progreso</option>
             <option value="Finalizado">Terminado</option>
-            <option value="Bloqueado">Bloqueado</option>
           </select>
         </div>
-        <div className="col-span-2">
-          <label className="block mb-2 text-sm font-medium text-gray-900">
-            Prioridad
-          </label>
-          <select
-            id="countries"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            value={taskInfo.prioridad}
-            onChange={(e) =>
-              setTaskInfo((prev) => ({ ...prev, prioridad: e.target.value }))
-            }
-          >
-            <option value={DEFAULT_SELECT_VALUE}>Elegir</option>
-            <option value="Baja">Baja</option>
-            <option value="Media">Media</option>
-            <option value="Alta">Alta</option>
-          </select>
-        </div>
-        <div className="col-span-2">
+        <div className="col-span-3">
           <label className="block mb-2 text-sm font-medium text-gray-900">
             Asignado
           </label>
           <select
             id="countries"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            value={taskInfo.asignado}
+            value={projectInfo.lider}
             onChange={(e) =>
-              setTaskInfo((prev) => ({ ...prev, asignado: e.target.value }))
+              setProjectInfo((prev) => ({ ...prev, asignado: e.target.value }))
             }
           >
             <option value={DEFAULT_SELECT_VALUE}>Elegir</option>
@@ -216,29 +198,34 @@ export default function TaskLayout({
             })}
           </select>
         </div>
-        <div className="col-span-6">
-          <label className="block mb-2 text-sm font-medium text-gray-900">
-            Descripción
-          </label>
-          <textarea
-            rows={4}
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-            onChange={(e) =>
-              setTaskInfo((prev) => ({ ...prev, descripcion: e.target.value }))
-            }
-            value={taskInfo.descripcion}
-          ></textarea>
-        </div>
       </div>
-      <div></div>
-      <div className="flex justify-end items-center gap-4 mt-8">
-        <Link href={`/tablero/${project_id}`}>Volver a tablero</Link>
-        <button
-          className="bg-sky-500	hover:bg-cyan-600 text-white font-bold py-1 px-4 rounded"
-          onClick={handleTaskSubmit}
-        >
-          Guardar
-        </button>
+      <div
+        className={`flex ${
+          createsProject ? "justify-end" : "justify-between"
+        } py-6 mt-8`}
+      >
+        {!createsProject && (
+          <Link
+            className="bg-zinc-200	hover:bg-zinc-100 text-black font-bold py-1 px-4 rounded "
+            href={`/tablero/${encodeURIComponent(router.query.id as string)}`}
+          >
+            VER TABLERO
+          </Link>
+        )}
+        <div className="flex">
+          <Link
+            className="bg-zinc-0	hover:bg-zinc-50 text-black font-bold py-1 px-4"
+            href={`/proyectos`}
+          >
+            Volver
+          </Link>
+          <button
+            className="bg-sky-500 hover:bg-cyan-600 text-white font-bold py-1 px-4 rounded"
+            onClick={handleProjectSubmit}
+          >
+            {createsProject ? "Guardar" : "Actualizar"}
+          </button>
+        </div>
       </div>
     </div>
   );
--- pages/proyecto/[id].tsx
+++ pages/proyecto/[id].tsx
@@ -1,169 +1,46 @@
-import Link from "next/link";
-import Router, { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import ProjectLayout from "@/components/ProjectLayout";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
-interface ProjectInfo {
-  nombre: string;
-  fechaInicio: string;
-  fechaFin: string;
-  id: string;
-  estado: string;
-  lider: string;
-}
-
-export default function ProjectLayout() {
+export default function UpdateTaskLayout() {
   const router = useRouter();
-  const [projectId, setProjectId] = useState<string>("");
-  const [invalidForm, setInvalidForm] = useState<boolean>(false);
-  const [error, setError] = useState<any>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [projectInfo, setProjectInfo] = useState<ProjectInfo>({
-    nombre: "",
-    fechaInicio: "",
-    fechaFin: "",
-    id: "",
-    estado: "",
-    lider: "",
-  });
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [projectId, setProjectId] = useState("");
 
   useEffect(() => {
-    setProjectId(router.query.id as string);
-    setIsLoading(true);
-
-    setProjectInfo({
-      id: "6",
-      lider: "Laura",
-      estado: "finalizado",
-      fechaInicio: "2023-09-20",
-      fechaFin: "2023-10-25",
-      nombre: "Proyecto F",
-    });
-    /*
-    const options = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    const url = `backend-api.com/proyectos/${router.query.id}`;
-
-    fetch(url, options)
-      .then((res) => res.json())
-      .then((res) => {
-        setProjectInfo(res);
-      })
-      .catch((err) => router.push("/error"));*/
+    if (router.isReady) {
+      console.log(router.query, "router.query");
+      setIsLoading(false);
+      setProjectId(router.query.id as string);
+    }
   }, [router.isReady]);
 
-  const handleUpdateProject = (e: any) => {
-    //e.preventDefault();
-
-    setIsLoading(true);
-
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(projectInfo),
-    };
-    const url = "backend-api.com/proyectos";
-
-    fetch(url, options)
-      .then((res) => res.json())
-      .then((res) => {
-        const { id } = res;
-        router.push(`/proyecto-actualizado/${id}`);
-      })
-      .catch((err) => router.push("/error"));
-  };
-
-  return (
-    <div className="w-screen mt-1">
-      <div className="flex ml-10 mr-20 border">
-        <img
-            src="../info40.png"
-            alt="info"
-            className="mt-12 mb-12 ml-10"
-
-        />
-        <h1 className="text-3xl font-bold mb-10 mt-12 ml-5">Información Del Proyecto -</h1>
-        <h1 className="text-3xl font-bold mb-10 mt-12 ml-2">{projectInfo?.nombre}</h1>
-      </div>
-      <div className="ml-10 mt-5 mr-20">
-        <p className="font-semibold mb-1 ml-20">Nombre proyecto</p>
-        <input
-          className="border w-2/3 px-1 mb-5 rounded ml-20"
-          type="text"
-          placeholder="Nombre..."
-          onChange={(e: any) =>
-            setProjectInfo({ ...projectInfo, nombre: e.target.value })
-          }
-          value={projectInfo?.nombre}
-        />
-      </div>
-      <div className="ml-10 mr-20">
-        <p className="font-semibold mb-1 ml-20">Fecha de inicio</p>
-        <input
-          className="border w-2/3 px-1 mb-5 rounded ml-20"
-          type="text"
-          placeholder="Fecha de inicio..."
-          disabled
-          value={projectInfo?.fechaInicio}
-        />
-      </div>
-      <div className="ml-10 mr-20">
-        <p className="font-semibold mb-1 ml-20">Fecha de finalización</p>
-        <input
-          className="border w-2/3 px-1 mb-5 rounded ml-20"
-          type="text"
-          placeholder="Fecha de finalización..."
-          onChange={(e: any) =>
-            setProjectInfo({ ...projectInfo, fechaFin: e.target.value })
-          }
-          value={projectInfo?.fechaFin}
-        />
-      </div>
-      <div className="flex mb-10 ml-10 mr-20">
-        <h1 className="font-semibold mr-1 ml-20">Estado:</h1>
-        <select
-          className="border rounded"
-          onChange={(e) =>
-            setProjectInfo({ ...projectInfo, estado: e.target.value })
-          }
-        >
-          <option value="Sin comenzar">Sin comenzar</option>
-          <option value="En progreso">En progreso</option>
-          <option value="Finalizado">Finalizado</option>
-        </select>
-
-        <h1 className="font-semibold mr-1 ml-40">Líder:</h1>
-        <select
-          className="border rounded"
-          onChange={(e) =>
-            setProjectInfo({ ...projectInfo, lider: e.target.value })
-          }
-        >
-          <option value="Ricardo">Ricardo</option>
-          <option value="Juan">Juan</option>
-          <option value="Francisco">Francisco</option>
-        </select>
-      </div>
-      <div className="flex ml-10 mr-20  py-6">
-
-        <Link
-            className="bg-zinc-200	hover:bg-zinc-100 text-black font-bold py-1 px-4 rounded ml-20 mr-80"
-            href={`/tablero/${encodeURIComponent(projectId)}`}
-        >
-            VER TABLERO
-        </Link>
-        <Link
-            className="bg-zinc-0	hover:bg-zinc-50 text-black font-bold py-1 px-4"
-            href={`/proyectos`}
-        >Volver
-        </Link>
-            <button className="bg-sky-500 hover:bg-cyan-600 text-white font-bold py-1 px-4 rounded" onClick={handleUpdateProject}>Actualizar</button>
+  if (isLoading) {
+    return (
+      <div class="text-center py-40">
+        <div role="status">
+          <svg
+            aria-hidden="true"
+            class="inline w-22 h-22 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+            viewBox="0 0 100 101"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+              fill="currentColor"
+            />
+            <path
+              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+              fill="currentFill"
+            />
+          </svg>
+          <span class="sr-only">Loading...</span>
         </div>
-    </div>
-  );
+        <h1 className="text-2xl font-semibold mt-5">Cargando...</h1>
+      </div>
+    );
+  }
+
+  return <ProjectLayout id={projectId} />;
 }
